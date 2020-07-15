01388bbf759b83102299c6bf9e31b089090bf05c
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-15 16:02:10 +0530

    Displaying Burger Content Dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-import Burger from './../../components/Burger/Burger';
+import Burger from "./../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      onion: 1,
+      cheese: 2,
+      tomato: 2,
+    },
+  };
   render() {
+    const { ingredients } = this.state;
     return (
       <div>
-        <Burger/>
+        <Burger ingredients={ingredients} />
       </div>
     );
   }
