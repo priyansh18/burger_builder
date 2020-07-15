275184b4bda52218269b515e3255fd78fd47b296
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-15 13:59:17 +0530

    Starter Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import Layout from './components/Layout/Layout';
+import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div className="App">
-      {/* <Layout>
+      <Layout>
         <BurgerBuilder/>
-      </Layout> */}
+      </Layout>
     </div>
   );
 }
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <div>
+        <div>Burgers</div>
+        <div>Build Controls</div>
+      </div>
+    );
+  }
+}
+
+export default BurgerBuilder;
