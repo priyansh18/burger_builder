083cc9f37dbf4b049765cf2f2da271f607a05335
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-15 18:41:21 +0530

    Setting up BurgerContols UI

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,17 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const Burger = ({ ingredients }) => {
   // console.log(ingredients);
   // object.keys converts object received from BurgerBuilder components into array
-  const transformedIngredients = Object.keys(ingredients).map((ingredientKey) => {
+  let transformedIngredients = Object.keys(ingredients).map((ingredientKey) => {
     return [...Array(ingredients[ingredientKey])].map((_, index) => {
       return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />;
     });
-  });
+  }).reduce((arr,el)=>{
+    return arr.concat(el)
+  },[]);
   console.log(transformedIngredients);
+  if(transformedIngredients.length===0){
+    transformedIngredients = <p>Please start adding Ingredients!</p>
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
