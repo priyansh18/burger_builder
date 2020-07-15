4cde076ee34dc6acbb099dad9bbf9211f4a80e8a
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-15 16:02:10 +0530

    Displaying Burger Content Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import classes from './Burger.module.css'
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const Burger = (props) => {
-  return ( <div className={classes.Burger}>
-    <BurgerIngredient type="bread-top"/>
-    <BurgerIngredient type="cheese"/>
-    <BurgerIngredient type="onion"/>
-    <BurgerIngredient type="bread-bottom"/>
-  </div> );
-}
- 
+const Burger = ({ ingredients }) => {
+  // console.log(ingredients);
+  // object.keys converts object received from BurgerBuilder components into array
+  const transformedIngredients = Object.keys(ingredients).map((ingredientKey) => {
+    return [...Array(ingredients[ingredientKey])].map((_, index) => {
+      return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />;
+    });
+  });
+  console.log(transformedIngredients);
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
 export default Burger;
