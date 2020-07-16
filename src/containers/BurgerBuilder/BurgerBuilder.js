f2e9d7cdcaf87b889b84c41c99d182409a359cb3
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-16 13:12:35 +0530

    Adding & Removing Burger Ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,21 +2,79 @@ import React, { Component } from "react";
 import Burger from "./../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENT_PRICE = {
+  salad: 30,
+  cheese: 40,
+  onion: 20,
+  tomato: 10,
+};
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 2,
-      onion: 1,
-      cheese: 2,
-      tomato: 3,
+      salad: 0,
+      onion: 0,
+      cheese: 0,
+      tomato: 0,
     },
+    totalPrice: 80,
   };
+  addIngredientHandler = (type) => {
+    // Update Ingredients
+    const updatedCount = this.state.ingredients[type] + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+
+    // Updating Price
+    const priceAddition = INGREDIENT_PRICE[type];
+    const newPrice = this.state.totalPrice + priceAddition;
+
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients,
+    });
+  };
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+
+    // Updating Price
+    const priceDeduction = INGREDIENT_PRICE[type];
+    const newPrice = this.state.totalPrice - priceDeduction;
+
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients,
+    });
+  };
+
   render() {
     const { ingredients } = this.state;
+    const disabledInfo = {
+      ...ingredients,
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <div>
         <Burger ingredients={ingredients} />
-        <BuildControls />
+        <BuildControls
+          onIngredientAdd={this.addIngredientHandler}
+          onIngredientRemove={this.removeIngredientHandler}
+          disabled={disabledInfo}
+        />
       </div>
     );
   }
