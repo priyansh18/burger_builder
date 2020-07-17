beb1781a46209573b29e033298990057307999ba
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-17 18:55:00 +0530

    Drawer Toggle & Backdrop

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,9 +2,9 @@ import React from "react";
 import burgerLogo from "../../assets/images/burger-logo.png";
 import classes from './Logo.module.css'
 
-const Logo = () => {
+const Logo = ({height}) => {
   return (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height:height}}>
       <img src={burgerLogo} alt="My Burger" />
     </div>
   );
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import NavigationItems from "./../NavigationItems/NavigationItems";
+import Logo from "./../../Logo/Logo";
+import classes from "./SideDrawer.module.css";
+import BackDrop from "./../../UI/BackDrop/BackDrop";
+
+const SideDrawer = ({ closed, show }) => {
+  // console.log("Open", show);
+  // console.log("Closed", closed);
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  console.log(attachedClasses);
+  if (show) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+  return (
+    <div>
+      <BackDrop show={show} clicked={closed} />
+      <div className={attachedClasses.join(" ")}>
+        <Logo height="11%" />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default SideDrawer;
