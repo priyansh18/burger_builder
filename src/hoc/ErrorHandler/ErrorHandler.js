2659da24ef92155922bc210333ab4f41063932a7
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-18 16:08:09 +0530

    Fetched Ingredients from firebase

--- src/hoc/ErrorHandler/ErrorHandler.js
+++ src/hoc/ErrorHandler/ErrorHandler.js
@@ -7,19 +7,24 @@ const ErrorHandler = (WrapperComponent, axios) => {
       error: null,
     };
 
-    componentDidMount() {
-      axios.interceptors.request.use((req) => {
+    componentWillMount() {
+      this.reqInterceptors = axios.interceptors.request.use((req) => {
         this.setState({
           error: null,
         });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptors = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount(){
+      axios.interceptors.request.eject(this.reqInterceptors)
+      axios.interceptors.response.eject(this.resInterceptors)
     }
 
     errorConfirmedHandler = () => {
