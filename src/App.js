68d429d52bef34c27b17fa6790dedd5e1b618884
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-19 13:27:56 +0530

    Setting up react-router and Navigating to Checkout Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './App.css';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Layout from './hoc/Layout/Layout';
-import Checkout from './containers/Checkout/Checkout';
+import React from "react";
+import "./App.css";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Layout from "./hoc/Layout/Layout";
+import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <BurgerBuilder/>
-        <Checkout/>
+        <Switch>
+          <Route exact path="/checkout" component={Checkout} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>
       </Layout>
     </div>
   );
