fb63b4008f114997ae8c885e42cae97c80fe4242
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-26 17:10:53 +0530

    SettingUp Auth Actions & Getting Token From Backend

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,2 +1,3 @@
 export { addIngredient, removeIngredient, initIngredients } from "./ingredient";
-export { purchaseBurger,purchaseInit,fetchOrders } from "./order";
+export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export { auth } from "./auth";
